2d8e1cfb79bf9c18e9e058c4eb88d50feacb3cf2
Shai Raz <shayy9999@gmail.com>
2023-04-08 23:35:40 +0300

    formatting

--- src/firebase/users.js
+++ src/firebase/users.js
@@ -1,36 +1,35 @@
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
-import {auth} from './config';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './config';
 
 export async function createUser(mail, password) {
-    console.log(mail);
-    console.log(password);
-    createUserWithEmailAndPassword(auth, mail, password)
-      .then((userCredential) => {
-        // Signed in
-        console.log("createUser");
-        const user = userCredential.user;
-        return user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
-  }
-  
-  export async function loginUser(mail, password) {
-    console.log(mail);
-    console.log(password);
-    signInWithEmailAndPassword(auth, mail, password)
-      .then((userCredential) => {
-        // Signed in
-        console.log("loginUser");
-        const user = userCredential.user;
-        console.log(user);
-        return user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
-  }
-  
+  console.log(mail);
+  console.log(password);
+  createUserWithEmailAndPassword(auth, mail, password)
+    .then((userCredential) => {
+      // Signed in
+      console.log("createUser");
+      const user = userCredential.user;
+      return user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
+
+export async function loginUser(mail, password) {
+  console.log(mail);
+  console.log(password);
+  signInWithEmailAndPassword(auth, mail, password)
+    .then((userCredential) => {
+      // Signed in
+      console.log("loginUser");
+      const user = userCredential.user;
+      console.log(user);
+      return user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
